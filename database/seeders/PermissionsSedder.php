ea17acb058fbdb2ba03a285bc88449a0fc849ec0
Lana <lanabexa@gmail.com>
2025-12-26 08:15:40 +0300

    roles and permissions created

---
+++ database/seeders/PermissionsSedder.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+class PermissionsSedder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+    // Roles
+    $adminRole = Role::firstOrCreate(['name' => 'admin']);
+    $userRole  = Role::firstOrCreate(['name' => 'user']);
+
+    // Permissions
+    $permissions = [
+    // Catalog
+        'manage books',
+        'manage authors',
+        'manage genres',
+        'manage languages',
+        'manage translators',
+
+        // Posts
+        'create posts',
+        'edit own posts',
+        'delete own posts',
+        'manage all posts',
+
+        // Comments
+        'create comments',
+        'edit own comments',
+        'delete own comments',
+        'manage comments',
+
+    ];
+
+    foreach ($permissions as $permission) {
+        Permission::firstOrCreate(['name' => $permission]);
+    }
+
+    // Assign permissions
+    
+    $adminRole->syncPermissions(Permission::all());
+    $userRole->syncPermissions([
+        'create posts',
+        'edit own posts',
+        'delete own posts',
+        'create comments', 
+        'edit own comments', 
+        'delete own comments',
+    ]);
+    }
+}
